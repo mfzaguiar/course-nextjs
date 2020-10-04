59ab3f95feca51ffdb4c10a574774059f27a3a4e
Matheus Aguiar <first_mf@hotmail.com>
2020-10-04 15:10:48 -0300

    next dynamic import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,53 @@
+import { GetServerSideProps } from 'next'
+
 import { Title } from '../styles/pages/Home'
 
-export default function Home() {
+interface IProduct {
+  id:string;
+  title:string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+
+async function handleSum() {
+    const math = (await import ('../lib/math')).default;
+
+    alert(math.sum(3, 5));
+}
+
   return (
     <div>
-      <Title>Hello world</Title>
+      <section>
+        <Title>Products</Title>
+        <ul>
+            {recommendedProducts.map(product => {
+            return   (
+                <li key={product.id}>
+                {product.title}
+                </li>
+            )
+            })}
+        </ul>
+      </section>
+
+      <button onClick={handleSum}>
+          Sum
+      </button>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async() => {
+  const response = await fetch('http://localhost:3333/recommended')
+  const recommendedProducts = await response.json();
+
+  return {
+        props:{
+           recommendedProducts 
+    }
+}
 }
